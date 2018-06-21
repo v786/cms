4f586b6b07380b6704ecc2c9118838ecb852ad4a
v786 <jahangirali786@gmail.com>
2018-06-21 14:48:20 +0530

    Added search using tags etc

--- src/Model/Entity/Article.php
+++ src/Model/Entity/Article.php
@@ -4,6 +4,9 @@ namespace App\Model\Entity;
 
 use Cake\ORM\Entity;
 
+// the Collection class
+use Cake\Collection\Collection;
+
 class Article extends Entity
 {
     protected $_accessible = [
@@ -11,4 +14,19 @@ class Article extends Entity
         'id' => false,
         'slug' => false,
     ];
+
+    protected function _getTagString()
+    {
+        if (isset($this->_properties['tag_string'])) {
+            return $this->_properties['tag_string'];
+        }
+        if (empty($this->tags)) {
+            return '';
+        }
+        $tags = new Collection($this->tags);
+        $str = $tags->reduce(function ($string, $tag) {
+            return $string . $tag->title . ', ';
+        }, '');
+        return trim($str, ', ');
+    }
 }
--- src/Model/Table/ArticlesTable.php
+++ src/Model/Table/ArticlesTable.php
@@ -10,6 +10,8 @@ use Cake\ORM\Table;
 // the Text class
 use Cake\Utility\Text;
 
+use Cake\ORM\Query;
+
 
 class ArticlesTable extends Table
 {
@@ -39,5 +41,61 @@ class ArticlesTable extends Table
             ->minLength('body', 10);
 
         return $validator;
+    }
+
+    public function findTagged(Query $query, array $options)
+    {
+        $columns = [
+            'Articles.id', 'Articles.user_id', 'Articles.title',
+            'Articles.body', 'Articles.published', 'Articles.created',
+            'Articles.slug',
+        ];
+
+        $query = $query
+            ->select($columns)
+            ->distinct($columns);
+
+        if (empty($options['tags'])) {
+            // If there are no tags provided, find articles that have no tags.
+            $query->leftJoinWith('Tags')
+                ->where(['Tags.title IS' => null]);
+        } else {
+            // Find articles that have one or more of the provided tags.
+            $query->innerJoinWith('Tags')
+                ->where(['Tags.title IN' => $options['tags']]);
+        }
+
+        return $query->group(['Articles.id']);
+    }
+
+    protected function _buildTags($tagString)
+    {
+        // Trim tags
+        $newTags = array_map('trim', explode(',', $tagString));
+        // Remove all empty tags
+        $newTags = array_filter($newTags);
+        // Reduce duplicated tags
+        $newTags = array_unique($newTags);
+
+        $out = [];
+        $query = $this->Tags->find()
+            ->where(['Tags.title IN' => $newTags]);
+
+        // Remove existing tags from the list of new tags.
+        foreach ($query->extract('title') as $existing) {
+            $index = array_search($existing, $newTags);
+            if ($index !== false) {
+                unset($newTags[$index]);
+            }
+        }
+        // Add existing tags.
+        foreach ($query as $tag) {
+            $out[] = $tag;
+        }
+        // Add new tags.
+        foreach ($newTags as $tag) {
+            $out[] = $this->Tags->newEntity(['title' => $tag]);
+        }
+        return $out;
     }
 }
